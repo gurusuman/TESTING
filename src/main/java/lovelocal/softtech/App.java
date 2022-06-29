715da165655107b793a91db062ce56c93ec6119f
root <root@ip-172-31-37-56.ap-south-1.compute.internal>
2022-06-29 05:59:18 +0000

    gu

---
+++ src/main/java/lovelocal/softtech/App.java
@@ -0,0 +1,13 @@
+package lovelocal.softtech;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/lovelocal/softtech/AppTest.java
@@ -0,0 +1,20 @@
+package lovelocal.softtech;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
